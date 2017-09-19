ca10ba1853d7b33755945ca01751460acdf027ef
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2017-09-19 22:00:21 +0200

    spring boot base application

---
+++ src/main/kotlin/pl/jp/s3uploader/Main.kt
@@ -0,0 +1,11 @@
+package pl.jp.s3uploader
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Main::class.java, *args)
+}
