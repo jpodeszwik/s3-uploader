c09c80248f742d4ed49e330ac9e5cab84d383828
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2017-09-23 00:57:22 +0200

    http basic auth

--- src/main/kotlin/pl/jp/s3uploader/S3UploaderProperties.kt
+++ src/main/kotlin/pl/jp/s3uploader/S3UploaderProperties.kt
@@ -9,4 +9,7 @@ class S3UploaderProperties {
     lateinit var s3AccessKey: String
     lateinit var s3SecretKey: String
     lateinit var s3Bucket: String
+
+    lateinit var username: String
+    lateinit var password: String
 }
