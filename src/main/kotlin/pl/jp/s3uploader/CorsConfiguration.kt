1a2439fb887ad080980d4d2ca5f91a9707981172
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2017-11-28 14:51:44 +0100

    Cors configuration

---
+++ src/main/kotlin/pl/jp/s3uploader/CorsConfiguration.kt
@@ -0,0 +1,22 @@
+package pl.jp.s3uploader
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class CorsConfiguration {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+            }
+        }
+    }
+}
